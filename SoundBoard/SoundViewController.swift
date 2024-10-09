a42a5c17ec809502782cc01b1915e40e01a62f98
Cristian <cristian.huillca@tecsup.edu.pe>
2024-10-09 18:43:50 -0500

    LABORATORIO TERMINADO

---
+++ SoundBoard/SoundViewController.swift
@@ -0,0 +1,112 @@
+//
+//  SoundViewController.swift
+//  SoundBoard
+//
+//  Created by Cristian H Gutierrez on 9/10/24.
+//
+
+import UIKit
+import AVFoundation
+
+class SoundViewController: UIViewController {
+
+    
+    @IBOutlet weak var grabarButton: UIButton!
+    @IBOutlet weak var reproducirButton: UIButton!
+    @IBOutlet weak var nombreTextField: UITextField!
+    @IBOutlet weak var agregarButton: UIButton!
+    
+    var grabarAudio:AVAudioRecorder?
+    var reproducirAudio:AVAudioPlayer?
+    var audioURL:URL?
+    
+    @IBAction func grabarTapped(_ sender: Any) {
+        if grabarAudio!.isRecording{
+            grabarAudio?.stop()
+            grabarButton.setTitle("GRABAR", for: .normal)
+            reproducirButton.isEnabled=true
+            agregarButton.isEnabled = true
+        }else{
+            grabarAudio?.record()
+            grabarButton.setTitle("DETENER", for: .normal)
+            reproducirButton.isEnabled = false
+            
+            
+            
+        }
+        
+    }
+    @IBAction func reproducirTapped(_ sender: Any) {
+        do{
+            try reproducirAudio=AVAudioPlayer(contentsOf: audioURL!)
+            reproducirAudio!.play()
+            print("Reproduciendo")
+        }catch {}
+        
+    }
+    @IBAction func agregarTapped(_ sender: Any) {
+        let context = ( UIApplication.shared.delegate as!AppDelegate).persistentContainer.viewContext
+        let grabacion = Grabacion(context:context)
+        grabacion.nombre = nombreTextField.text
+        grabacion.audio = NSData(contentsOf: audioURL!)! as Data
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController!.popViewController(animated: true)
+    }
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configurarGrabacion()
+        reproducirButton.isEnabled=false
+        agregarButton.isEnabled=false
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func configurarGrabacion() {
+        do {
+            // creando sesión de audio
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: [])
+            try session.overrideOutputAudioPort(.speaker)
+            try session.setActive(true)
+
+            // creando dirección para el archivo de audio
+            let basePath: String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            let pathComponents = [basePath, "audio.m4a"]
+            audioURL = NSURL.fileURL(withPathComponents: pathComponents)!
+
+            // impresión de ruta donde se guardan los archivos
+            print("*********************")
+            print(audioURL!)
+            print("*********************")
+
+            // crear opciones para el grabador de audio
+            var settings: [String: AnyObject] = [:]
+            settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC) as AnyObject?
+            settings[AVSampleRateKey] = 44100.0 as AnyObject?
+            settings[AVNumberOfChannelsKey] = 2 as AnyObject?
+
+            // crear el objeto de grabación de audio
+            grabarAudio = try AVAudioRecorder(url: audioURL!, settings: settings)
+            grabarAudio!.prepareToRecord()
+        } catch let error as NSError {
+            print(error)
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
